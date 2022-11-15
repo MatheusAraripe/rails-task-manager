1beb71636e33bcc2fac25249f1aaf5a6d10976e3
Matheus Araripe <matheus.alc2001@gmail.com>
2022-11-15 15:52:30 -0300

    add default value to the model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_15_175951) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_15_185002) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
